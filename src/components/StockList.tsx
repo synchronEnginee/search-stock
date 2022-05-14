fb53c0b3a811d875027dbab518d8dac3d0f8205f
synchronEnginee <te2.9niku.41cd@gmail.com>
2022-05-14 17:30:05 +0900

    StockList template made

--- src/components/StockList.tsx
+++ src/components/StockList.tsx
@@ -4,13 +4,43 @@ import { css } from "@emotion/react";
 
 const StockList = () => {
   const styles = css({
+    width: "60vw",
+    height: "100%",
+    margin: "0 auto",
     "font-size": "30px",
+    border: "1px solid black",
 
-    "&:hover": {
-      color: "red",
+    "tr th": {
+      "font-weight": "bold",
+      background: "#fff5e5",
+      border: "1px double black",
+    },
+    "tr td": {
+      border: "1px solid black",
     },
   });
-  return <div css={styles}>StockList</div>;
+  return (
+    <table css={styles}>
+      <tr>
+        <th>銘柄名</th>
+        <th>銘柄コード</th>
+        <th>株価</th>
+        <th>PER</th>
+      </tr>
+      <tr>
+        <td>任天堂</td>
+        <td>7974</td>
+        <td>57250</td>
+        <td>19.75</td>
+      </tr>
+      <tr>
+        <td>キーエンス</td>
+        <td>6861</td>
+        <td>51020</td>
+        <td>----</td>
+      </tr>
+    </table>
+  );
 };
 
 export default StockList;
