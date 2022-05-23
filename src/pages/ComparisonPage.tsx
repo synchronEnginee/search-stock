a6d6a023708c96ecce8b48cb60ee9f1d2a37250e
synchronEnginee <te2.9niku.41cd@gmail.com>
2022-05-23 22:07:43 +0900

    todo getStockCompare type fix

--- src/pages/ComparisonPage.tsx
+++ src/pages/ComparisonPage.tsx
@@ -1,7 +1,13 @@
 /** @jsxImportSource @emotion/react */
-import React from "react";
+import React, { useEffect } from "react";
 import { css } from "@emotion/react";
 import { Link } from "react-router-dom";
+import axios, {
+  AxiosResponse,
+  AxiosRequestConfig,
+  AxiosPromise,
+  AxiosError,
+} from "axios";
 import {
   ComparisonStock,
   ComparisonStockProps,
@@ -16,22 +22,64 @@ const comparisonStocks: { stocks: Array<ComparisonStockProps> } = {
   ],
 };
 
+const styles = css({
+  width: "60vw",
+  height: "100%",
+  margin: "0 auto",
+  "font-size": "30px",
+  border: "1px solid black",
+  "tr:nth-child(odd)": {
+    "background-color": "rgb(255, 255, 128)",
+  },
+  "tr th": {
+    "font-weight": "bold",
+    background: "#fff5e5",
+    border: "1px double black",
+  },
+});
+
+const stockUrl = "https://127.0.0.1:5000/stock";
+
+// 項目に欠けがあった場合でもエラーが出るよう外部で定義
+interface StockGetResponse {
+  name: string;
+  code: string;
+  price: string;
+  per: string;
+}
+
+interface IErrorResponse {
+  error: string;
+}
+
+// any入っちゃったので要改善
+type FetchStock = () => AxiosPromise<StockGetResponse> | Promise<Error>;
+
+// サーバからの応答の形式を渡す
+// TODO: useEffect内、ファイルを切り出す
+const getStock = () => {
+  const getStockCompare: FetchStock = async () => {
+    await axios
+      .get("https://127.0.0.1:5000/stock")
+      .then((res: AxiosResponse<StockGetResponse>) => {
+        // res.data.tokenはstring
+        console.log(`token: ${res.data.code}`);
+        return res;
+      })
+      // エラー応答の構造を明示する
+      .catch((e: AxiosError<IErrorResponse>) => {
+        if (e.response !== undefined) {
+          // e.response.data.errorはstring
+          console.log(e.response.data.error);
+          return new Error(e.response.data.error);
+        }
+      });
+  };
+  return getStockCompare;
+};
+
 const ComparisonPage = () => {
-  const styles = css({
-    width: "60vw",
-    height: "100%",
-    margin: "0 auto",
-    "font-size": "30px",
-    border: "1px solid black",
-    "tr:nth-child(odd)": {
-      "background-color": "rgb(255, 255, 128)",
-    },
-    "tr th": {
-      "font-weight": "bold",
-      background: "#fff5e5",
-      border: "1px double black",
-    },
-  });
+  useEffect(() => {});
   return (
     <div>
       銘柄比較
