5e29b44b22298f6c0384581e56dbce876b3ebdda
synchronEnginee <te2.9niku.41cd@gmail.com>
2022-06-01 22:02:22 +0900

    useSWR add FallStockPage

--- src/pages/FallStockPage.tsx
+++ src/pages/FallStockPage.tsx
@@ -3,28 +3,40 @@ import React from 'react';
 import { css } from '@emotion/react';
 import { Link } from 'react-router-dom';
 import FallStock, { FallStockProps } from 'components/FallStock';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import useSWR from 'swr';
 
-const fallStocks: { stocks: Array<FallStockProps> } = {
-  stocks: [
-    {
-      name: '任天堂',
-      code: '7974',
-      price: '58000',
-      stockFall: '20%',
-      stockTargetPrice: '80000',
-    },
-    {
-      name: 'キーエンス',
-      code: '6861',
-      price: '49000',
-      stockFall: '30%',
-      stockTargetPrice: '60000',
-    },
-  ],
-};
+// バックエンドから取得する(useSWRを使用する)
+// const fallStocks: { stocks: Array<FallStockProps> } = {
+//   stocks: [
+//     {
+//       name: '任天堂',
+//       code: '7974',
+//       price: '58000',
+//       stockFall: '20%',
+//       stockTargetPrice: '80000',
+//     },
+//     {
+//       name: 'キーエンス',
+//       code: '6861',
+//       price: '49000',
+//       stockFall: '30%',
+//       stockTargetPrice: '60000',
+//     },
+//   ],
+// };
+
+interface FallStockGetResponse extends AxiosResponse {
+  data: FallStockProps[];
+}
+
+const fallStockUrl = 'http://127.0.0.1:5000';
+
+const fetcher = (url: string) =>
+  axios.get<FallStockProps, FallStockGetResponse>(url).then((res) => res.data);
 
 const FallListPage = () => {
-  //   const [stocks, setStocks] = useState<FallStockProps[]>([]);
+  const { data } = useSWR(fallStockUrl, fetcher);
   const styles = css({
     width: '60vw',
     height: '100%',
@@ -49,15 +61,16 @@ const FallListPage = () => {
           <th>下落率</th>
           <th>目標株価</th>
         </tr>
-        {fallStocks.stocks.map((stock) => (
-          <FallStock
-            name={stock.name}
-            code={stock.code}
-            price={stock.price}
-            stockFall={stock.stockFall}
-            stockTargetPrice={stock.stockTargetPrice}
-          />
-        ))}
+        {data &&
+          data.map((stock) => (
+            <FallStock
+              name={stock.name}
+              code={stock.code}
+              price={stock.price}
+              stockFall={stock.stockFall}
+              stockTargetPrice={stock.stockTargetPrice}
+            />
+          ))}
       </table>
     </>
   );
